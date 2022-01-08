880874e5b32d4f56849e6c02f155af7a2b794fc8
DmitryTsepelev <dmitry.a.tsepelev@gmail.com>
2022-01-08 14:11:57 +0300

    Cleanup day12

--- day12/solution.hs
+++ day12/solution.hs
@@ -10,13 +10,13 @@ type Cave = String
 type Connection = (Cave, Cave)
 
 lineToPair :: [String] -> Connection
-lineToPair arr = (arr!!0, arr!!1)
+lineToPair arr = (head arr, arr!!1)
 
 directionsFrom :: Cave -> [Connection] -> [Cave]
 directionsFrom cave [] = []
 directionsFrom cave ((from, to):restConnections)
-  | cave == from = [to] ++ restDirections
-  | cave == to = [from] ++ restDirections
+  | cave == from = to:restDirections
+  | cave == to = from:restDirections
   | otherwise = restDirections
   where restDirections = directionsFrom cave restConnections
 
@@ -24,42 +24,41 @@ isBigCave :: Cave -> Bool
 isBigCave = all isUpper
 
 allowVisitEachSmallCaveOnlyOnce :: Cave -> [Cave] -> Bool
-allowVisitEachSmallCaveOnlyOnce currentCave currentPath = not(elem currentCave currentPath) || isBigCave currentCave
+allowVisitEachSmallCaveOnlyOnce currentCave currentPath = (currentCave `notElem` currentPath) || isBigCave currentCave
 
 canVisitCaveOnceMore :: [Cave] -> Bool
 canVisitCaveOnceMore currentPath =
-  all (\cave -> (length $ filter (==cave) currentPath) == 1) smallCaves
-  where smallCaves = filter (\cave -> not(isBigCave cave)) currentPath
+  all (\cave -> length (filter (==cave) currentPath) == 1) smallCaves
+  where smallCaves = filter (not . isBigCave) currentPath
 
 allowVisitOneSmallCaveTwice :: Cave -> [Cave] -> Bool
 allowVisitOneSmallCaveTwice currentCave currentPath
   | isBigCave currentCave = True
   | currentCave == "start" && elem currentCave currentPath = False
-  | not(elem currentCave currentPath) = True
+  | currentCave `notElem` currentPath = True
   | canVisitCaveOnceMore currentPath = True
   | otherwise = False
 
 findNextPaths :: Cave -> [Cave] -> [Connection] -> (Cave -> [Cave] -> Bool) -> [[Cave]]
 findNextPaths "end" currentPath _ _ = [currentPath ++ ["end"]]
-findNextPaths currentCave currentPath connections alllowVisit
-  | alllowVisit currentCave currentPath = foldl (\acc nextCave -> acc ++ findNextPaths nextCave newPath connections alllowVisit) [] directions
+findNextPaths currentCave currentPath connections canVisitCave
+  | canVisitCave currentCave currentPath =
+    foldl (\acc nextCave -> acc ++ findNextPaths nextCave newPath connections canVisitCave) [] directions
   | otherwise = []
   where
     directions = directionsFrom currentCave connections
-    newPath = (currentPath ++ [currentCave])
+    newPath = currentPath ++ [currentCave]
 
 findAllPaths :: [Connection] -> (Cave -> [Cave] -> Bool) -> [[Cave]]
-findAllPaths connections predicate = findNextPaths "start" [] connections predicate
+findAllPaths = findNextPaths "start" []
 
 main = do
   content <- readFile "input.txt"
-  let strings = lines(content)
-  let connections = map (\line -> lineToPair (wordsWhen (=='-') line)) strings
+  let strings = lines content
+  let connections = map (lineToPair . wordsWhen (=='-')) strings
 
-  let solution1 = length $ findAllPaths connections (\currentCave currentPath -> allowVisitEachSmallCaveOnlyOnce currentCave currentPath)
-  putStrLn("Solution 1 is " ++ show(solution1))
+  let solution1 = length $ findAllPaths connections allowVisitEachSmallCaveOnlyOnce
+  putStrLn("Solution 1 is " ++ show solution1)
 
-  print $ canVisitCaveOnceMore ["a", "a"]
-
-  let solution2 = length $ findAllPaths connections (\currentCave currentPath -> allowVisitOneSmallCaveTwice currentCave currentPath)
-  putStrLn("Solution 2 is " ++ show(solution2))
+  let solution2 = length $ findAllPaths connections allowVisitOneSmallCaveTwice
+  putStrLn("Solution 2 is " ++ show solution2)
